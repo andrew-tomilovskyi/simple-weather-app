22a42e993fd68c2f5deee78e039c8f902427d8bc
Andrii Tomilovskyi <andrex2091@gmail.com>
2021-12-06 13:25:43 +0200

    - added methods to get weather info from API & database

--- app/src/main/java/com/tom/simpleweatherapp/MainActivity.kt
+++ app/src/main/java/com/tom/simpleweatherapp/MainActivity.kt
@@ -8,7 +8,15 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.*
+import com.tom.simpleweatherapp.api.WeatherResponse
+import com.tom.simpleweatherapp.api.WeatherService
+import com.tom.simpleweatherapp.db.CityModel
 import com.tom.simpleweatherapp.db.MyDatabase
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.util.*
 
 class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
@@ -52,7 +60,101 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         // p.s. the app will crash if the location permission is not granted
         getCurrentLocation()
 
+        // setting the button to get weather info for the selected city
+        button.setOnClickListener {
+            btnListener()
+        }
 
+    }
+
+    private fun btnListener() {
+        if (spinner.selectedItem == "Current location") {
+            var city: String?
+            database.cityModelDao().getCity(currentCity).observe(this, { retrievedCity ->
+                city = retrievedCity
+                if (retrievedCity != null) {
+                    getWeatherInfoFromDB(city, database)
+                } else {
+                    getWeatherInfoFromApi(currentCity, database)
+                }
+            })
+        } else {
+            val selectedCity = spinner.selectedItem.toString()
+            var city: String?
+            database.cityModelDao().getCity(selectedCity).observe(this, { retrievedCity ->
+                city = retrievedCity
+                if (retrievedCity != null) {
+                    getWeatherInfoFromDB(city, database)
+                } else {
+                    getWeatherInfoFromApi(selectedCity, database)
+                }
+            })
+        }
+    }
+
+    private fun getWeatherInfoFromDB(cityName: String?, database: MyDatabase) {
+
+        var temperature: String
+        var weather: String
+
+        if (cityName != null) {
+            database.cityModelDao().getTemperature(cityName)
+                .observe(this, { retrievedTemperature ->
+                    temperature = retrievedTemperature.toString()
+                    tvResultsTemperature?.text = getString(R.string.tv_temperature_results, temperature)
+                })
+        }
+
+        if (cityName != null) {
+            database.cityModelDao().getWeatherConditions(cityName)
+                .observe(this, { retrievedWeather ->
+                    weather = retrievedWeather
+                    tvResultsWeather?.text = getString(R.string.tv_weather_results, weather)
+                })
+        }
+    }
+
+    // method to get the current temperature and weather conditions from API
+    private fun getWeatherInfoFromApi(city: String, database: MyDatabase) {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BaseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val service = retrofit.create(WeatherService::class.java)
+        val call = service.getCurrentWeatherData(city, units, AppId)
+        var responseTemperature: Float
+        var responseWeather: String? = ""
+
+        call.enqueue(object : Callback<WeatherResponse> {
+            override fun onResponse(
+                call: Call<WeatherResponse>,
+                response: Response<WeatherResponse>
+            ) {
+                if (response.code() == 200) {
+                    val weatherResponse = response.body()!!
+
+                    responseTemperature = weatherResponse.main!!.temp
+                    responseWeather = weatherResponse.weather[0].main
+
+                    // putting a new CityModel class into our database
+                    // including: city name, temperature, weather conditions
+                    Thread {
+                        database.cityModelDao()
+                            .insert(CityModel(city, responseTemperature, responseWeather))
+                    }.start()
+                }
+            }
+
+            override fun onFailure(call: Call<WeatherResponse>, t: Throwable) {
+                tvResultsTemperature!!.text = t.message
+            }
+        })
+    }
+
+    companion object {
+        var BaseUrl = "https://api.openweathermap.org/"
+        var AppId = "52287f86c593537704d9fa13a7e5d640"
+        var units = "metric"
     }
 
     private fun getCurrentLocation() {
