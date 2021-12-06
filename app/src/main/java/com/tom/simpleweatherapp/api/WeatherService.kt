eb2d8c1f9ac7bdc2da4b1aca782a9e4ab3698eeb
Andrii Tomilovskyi <andrex2091@gmail.com>
2021-12-06 13:02:21 +0200

    - added model class fo response from api - added service class to construct queries

---
+++ app/src/main/java/com/tom/simpleweatherapp/api/WeatherService.kt
@@ -0,0 +1,12 @@
+package com.tom.simpleweatherapp.api
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherService {
+
+    @GET("data/2.5/weather?")
+    fun getCurrentWeatherData(@Query("q") city: String, @Query("units") units: String, @Query("APPID") app_id: String): Call<WeatherResponse>
+
+}
